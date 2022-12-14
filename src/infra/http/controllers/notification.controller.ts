d9fe4125d6bb7056ca1cb319349f3df8e08128db
Arthur Akira <arthurakira.dev@gmail.com>
2022-12-14 18:43:26 -0300

    feat: add swagger decorators to dto and controller

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SendNotification } from '../../../app/use-cases/send-notification/send-notification.use-case';
 import { CreateNotificationDTO } from '../dtos/create-notification.dto';
 import { BadRequestInterceptor } from '../interceptors/bad-request-interceptor';
 
+@ApiTags('notifications')
 @Controller('notifications')
 export class NotificationsController {
   private readonly sendNotification: SendNotification;
@@ -13,6 +15,15 @@ export class NotificationsController {
 
   @Post()
   @UseInterceptors(BadRequestInterceptor)
+  @ApiOperation({ summary: 'Create a Notification' })
+  @ApiResponse({
+    status: 201,
+    description: 'Notification Created',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Params',
+  })
   async create(@Body() body: CreateNotificationDTO) {
     const { category, content, recipientId } = body;
 
--- src/infra/http/dtos/create-notification.dto.ts
+++ src/infra/http/dtos/create-notification.dto.ts
@@ -1,15 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationDTO {
+  @ApiProperty({
+    name: 'content',
+    example: 'valid content',
+    description: 'The content of the notification',
+    required: true,
+    minLength: 5,
+    maxLength: 240,
+  })
   @IsString()
   @IsNotEmpty()
   @Length(5, 240)
   content: string;
 
+  @ApiProperty({
+    name: 'category',
+    example: 'valid category',
+    description: 'The category of the notification',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   category: string;
 
+  @ApiProperty({
+    name: 'recipientId',
+    example: '1418e8b0-7bf7-11ed-a1eb-0242ac120002',
+    description: 'The recipient id from the notification. Must be an UUID',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @IsUUID()
