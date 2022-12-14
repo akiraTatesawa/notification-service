a04bb78e6519cef1333d89c4b3d38c1729da5e26
Arthur Akira <arthurakira.dev@gmail.com>
2022-12-14 18:14:44 -0300

    feat: add bad request interceptor

--- src/infra/http/dtos/create-notification.dto.ts
+++ src/infra/http/dtos/create-notification.dto.ts
@@ -1,13 +1,16 @@
-import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationDTO {
+  @IsString()
   @IsNotEmpty()
   @Length(5, 240)
   content: string;
 
+  @IsString()
   @IsNotEmpty()
   category: string;
 
+  @IsString()
   @IsNotEmpty()
   @IsUUID()
   recipientId: string;
